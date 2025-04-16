1421e638ba5c5f1b05e5bb0b25058210bbc940a1
Arthur Gomes <juarhotmail295@gmail.com>
2025-04-16 15:25:00 -0300

    Adicionando conteúdo

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+app.UseDefaultFiles(); // Serve index.html por padrão
+app.UseStaticFiles(); // Permite servir arquivos da pasta wwwroot
 app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/sobre", async context =>
+{
+await context.Response.SendFileAsync("wwwroot/sobre.html");
+});
 app.Run();
--- obj/Debug/net9.0/MinhaPrimeiraPagina.AssemblyInfo.cs
+++ obj/Debug/net9.0/MinhaPrimeiraPagina.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaPrimeiraPagina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6edf45deb17e9753245be6311ed86d5ea0f490b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8674ac8096dd81ff6a1c12b2a69d1e2b9742e7c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaPrimeiraPagina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaPrimeiraPagina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
