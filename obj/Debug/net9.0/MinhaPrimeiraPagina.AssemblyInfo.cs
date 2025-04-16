a3dda69866dc0d387b97277cbcf29c9679b70bd5
joaobarto <joaobfn12@gmail.com>
2025-04-16 16:47:14 -0300

    Att

--- obj/Debug/net9.0/MinhaPrimeiraPagina.AssemblyInfo.cs
+++ obj/Debug/net9.0/MinhaPrimeiraPagina.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaPrimeiraPagina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1421e638ba5c5f1b05e5bb0b25058210bbc940a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166c64fbe5d4d8b46fdb1eddc1fafd25169324d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaPrimeiraPagina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaPrimeiraPagina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
